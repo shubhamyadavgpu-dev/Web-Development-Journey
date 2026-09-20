f51a54ccc60209799e077c253fdd713d034c673b
Shubham yadav <shubhamyadavgpu@gmail.com>
2026-09-20 19:05:54 +0530

    NODE Files

---
+++ App.js
@@ -0,0 +1,23 @@
+// let n = 5;
+// for(let i = 0 ; i < n ; i++){
+//     console.log("Hello World!",i);
+// }
+
+// let a = process.argv;
+// for(let i = 2 ; i <= a.length ; i++){
+//     console.log("Hello to ,", a[i]);
+// }
+
+// const some = require("./Math");
+// console.log(some);
+//-----------------------------------------------------------
+// const info = require("./Fruits");
+// console.log(info);
+// console.log(info[0]);
+// console.log(info[0].name);
+
+//---------------------------------------------------------------------
+
+import { sum, mul } from "./Math.js";
+console.log("Sum is :", sum(1, 72));
+console.log("Mul is : ", mul(1, 72));
